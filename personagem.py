d7a0f0fb0e3bc975cb3acbac3f20473eb075f9c7
Daniel <danielzacheu2015@gmail.com>
2025-11-11 03:12:09 -0300

    v0.4 - criação da classe heroi, cenario inicial, itens basicos, criação dos inimigos, batalhas, drop de loot e melhorias gerais.

--- personagem.py
+++ personagem.py
@@ -1,28 +1,31 @@
+import math
+
 class Personagem:
     """
     A classe Personagem representa um personagem genérico em um jogo.
     """
-    def __init__(self, nome, vida_max=100, ataque=1, defesa=5, nivel=1, inventario=[]):
+    def __init__(self, nome, vida_max=100, ataque=1, defesa=5, nivel=1, experiencia=0, inventario={}, dialogos={}):
         self.nome = nome
         self.vida_max = vida_max
         self.vida = vida_max
         self.ataque = ataque
         self.defesa = defesa
         self.nivel = nivel
+        self.experiencia = experiencia
         self.inventario = inventario
+        self.dialogos = dialogos
 
-    def upgrade_vida(self, incremento=10):
+    def dar_vida(self, acres=10):
         """
         Aumenta a vida do personagem. O valor padrão de incremento é 10.
         """
-        if self.vida + incremento > self.vida_max:
+        if self.vida + acres > self.vida_max:
             self.vida = self.vida_max
         else:
-            self.vida += incremento
-        print(f'Vida de {self.nome} após upgrade: {self.vida}')
+            self.vida += acres
+        print(f'Vida atual de {self.nome}: {self.vida}')
 
-
-    def downgrade_vida(self, decres=10):
+    def retirar_vida(self, decres=10):
         """
         Reduz a vida do personagem, garantindo que não fique negativa.
         """
@@ -30,6 +33,21 @@ class Personagem:
             self.vida -= decres
         else:
             self.vida = 0
+        print(f'Vida atual de {self.nome}: {self.vida}')
+
+    def upgrade_vida_max(self, acres=10):
+        """
+        Aumenta a vida máxima do personagem. O valor padrão de incremento é 10.
+        """    
+        self.vida_max += acres
+        print(f'Vida de {self.nome} após upgrade: {self.vida}')
+
+    def downgrade_vida_max(self, descres=10):
+        """
+        Reduz a vida máxima do personagem, garantindo que não fique negativa.
+        """
+        if self.vida_max > descres:
+            self.vida_max -= descres
         print(f'Vida de {self.nome} após downgrade: {self.vida}')
 
     def update_nome(self, nome_editado):
@@ -38,8 +56,25 @@ class Personagem:
         """
         self.nome = nome_editado
 
+    def mostrar_vida(self):
+        tamanho_barra = 20
+        vida_atual = math.ceil((self.vida/self.vida_max)*tamanho_barra)
+        vazio = tamanho_barra - vida_atual
+        return f"[{'█' * vida_atual}{'░' * vazio}] {self.vida}/{self.vida_max} PV"
+
     def listar_inventario(self):
-        """ Mostra os itens presentes no inventário do personagem. """
+        """ 
+        Mostra os itens presentes no inventário do personagem.
+        """
+        if not self.inventario:
+            print("O inventário está vázio!")
+            return
+
+        for i, item in enumerate(sorted(self.inventario), start=1):
+            print(f"{i}. {item}")
+
+    def dialogar(self):
+        pass
 
     def __str__(self):
         return f'Personagem: {self.nome}, Vida máxima: {self.vida_max}, Vida atual: {self.vida}, Ataque: {self.ataque}, Defesa: {self.defesa}, Nível: {self.nivel}, Inventário: {self.inventario}'
---
+++ utils.py
@@ -0,0 +1,25 @@
+import json
+
+def limpar_terminal():
+    print("\033[H\033[2J", end="")
+    print("\033[H\033[3J", end="")
+    print("\033c", end="")
+
+def enter_continuar():
+    input("\nPressione Enter para continuar...")
+
+def carregar_itens():
+    try:
+        with open("data/itens.json", encoding='utf-8') as f:
+            return json.load(f)
+    except json.JSONDecodeError:
+        return {}
+    return{}
+
+def carregar_personagens():
+    try:
+        with open("data/personagens.json", encoding='utf-8') as f:
+            return json.load(f)
+    except json.JSONDecodeError:
+        return {}
+    return{}
